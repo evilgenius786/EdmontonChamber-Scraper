608ad193b75fb87d3be2ae7a0a749cddf13974f1
evilgenius786 <786hassan777@gmail.com>
2025-08-21 11:32:29 +0500

    Update

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from bs4 import BeautifulSoup
 from curl_cffi import requests
 import pandas as pd
 import glob
+from threading import Thread, Semaphore
+
 # import requests
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
 )
+thread_count=10
+semaphore = Semaphore(thread_count)
 def process_company(company_url):
     file_name= './json_/'+company_url.split('/')[-1] + '.json'
     if isfile(file_name):
@@ -28,10 +32,10 @@ def process_company(company_url):
         'url': company_url,
         'logo': company_soup.find('img', {'itemprop': 'logo'})['src'] if company_soup.find('img', {'itemprop': 'logo'}) else None,
         'name': company_soup.find('meta',{'itemprop':'name'}).get('content', '').strip(),
-        'streetAddress': company_soup.find('span', {'itemprop': 'streetAddress'}).text.strip(),
+        'streetAddress': company_soup.find('span', {'itemprop': 'streetAddress'}).text.strip() if company_soup.find('span', {'itemprop': 'streetAddress'}) else None,
         'addressLocality': company_soup.find('span', {'itemprop': 'addressLocality'}).text.strip(),
         'addressRegion': company_soup.find('span', {'itemprop': 'addressRegion'}).text.strip(),
-        'postalCode': company_soup.find('span', {'itemprop': 'postalCode'}).text.strip(),
+        'postalCode': company_soup.find('span', {'itemprop': 'postalCode'}).text.strip() if company_soup.find('span', {'itemprop': 'postalCode'}) else None,
         'telephone': ', '.join([telephone.find('a')['href'].replace('tel:','') for telephone in company_soup.find_all('li', {'class': 'list-group-item gz-card-phone'})]),
         'faxNumber': ', '.join([fax.find('a')['href'].replace('tel:','') for fax in company_soup.find_all('li', {'class': 'list-group-item gz-card-fax'})]) if company_soup.find_all('li', {'class': 'list-group-item gz-card-fax'}) else None,
         'website': company_soup.find('li', {'class': 'list-group-item gz-card-website'}).find('a')['href'] if company_soup.find('li', {'class': 'list-group-item gz-card-website'}) else None,
@@ -46,7 +50,7 @@ def process_company(company_url):
         'reptitle': company_soup.find('div', {'class': 'gz-member-reptitle'}).text.strip() if company_soup.find('div', {'class': 'gz-member-reptitle'}) else None,
         'repphone': company_soup.find('span', {'class': 'gz-rep-phone-num'}).text.strip() if company_soup.find('span', {'class': 'gz-rep-phone-num'}) else None,
     }
-    logging.debug(json.dumps(data))
+    logging.info(json.dumps(data))
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -55,6 +59,7 @@ def main():
     logo()
     makedirs('json_', exist_ok=True)
     logging.info("Starting to process listings...")
+    threads = []
     for alpha in ['0-9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
         logging.info(f"Processing listings for letter: {alpha}")
@@ -65,7 +70,13 @@ def main():
         for h5 in listing_soup.find_all('h5'):
             company_url = h5.find('a').get('href')
             if company_url:
-                process_company(company_url)
+                # process_company(company_url)
+                thread  = Thread(target=process_company, args=(company_url,))
+                thread.start()
+                threads.append(thread)
+        for thread in threads:
+            thread.join()
+        export_csv()
 def export_csv():
     files = glob.glob('./json_/*.json')
     logging.info(f"Found {len(files)} JSON files to export to CSV.")
@@ -86,11 +97,11 @@ def get_soup(url):
 
 
 def get_request(url):
-    res = requests.get(
-        url=url,
-        impersonate='chrome'
-        )
-    return res
+    with semaphore:
+        return requests.get(
+            url=url,
+            impersonate='chrome'
+            )
 
 def logo():
     print(rf"""
@@ -100,13 +111,15 @@ def logo():
 |  __|  / _` || '_ ` _ \  / _ \ | '_ \ | __| / _ \ | '_ \  | |    | '_ \  / _` || '_ ` _ \ | '_ \  / _ \| '__|
 | |___ | (_| || | | | | || (_) || | | || |_ | (_) || | | | | \__/\| | | || (_| || | | | | || |_) ||  __/| |   
 \____/  \__,_||_| |_| |_| \___/ |_| |_| \__| \___/ |_| |_|  \____/|_| |_| \__,_||_| |_| |_||_.__/  \___||_|   
-_______________________________________________________________________________________________________________
+===============================================================================================================
                             edmontonchamber.com - Edmonton Chamber of Commerce
                                 scraper by github.com@evilgeniua786
-_______________________________________________________________________________________________________________
-[+] JSON Based response
-
 ===============================================================================================================
+[+] JSON Based response
+[+] CSV Export
+[+] Data Scraped from https://business.edmontonchamber.com/list/searchalpha/
+[+] Resumable
+_______________________________________________________________________________________________________________
 """)
     time.sleep(1)
 if __name__ == '__main__':
